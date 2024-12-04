1e73f087f79986229e6d9e91433c01e72441cc7c
Maxiee <maxieewong@gmail.com>
2024-12-05 06:06:55 +0800

    Add functions to manage module-specific data paths in data.py

--- module/data/data.py
+++ module/data/data.py
@@ -14,6 +14,15 @@ def get_data_path() -> Path:
     return data_path
 
 
+def get_module_data_path(module_name: str) -> Path:
+    return data_path / module_name
+
+
+def make_sure_module_data_path_exists(module_name: str):
+    module_data_path = get_module_data_path(module_name)
+    module_data_path.mkdir(exist_ok=True)
+
+
 def init_data_module():
     # Create the data directory if it doesn't exist
     data_path.mkdir(exist_ok=True)
