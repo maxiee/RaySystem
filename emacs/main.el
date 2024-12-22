265f41f65155ec1f2b4b9b72eb3f11b2e4a26a59
Maxiee <maxieewong@gmail.com>
2024-12-22 12:44:59 +0800

    Refactor loading of Emacs Lisp files to dynamically load all .el files in the current directory

--- emacs/main.el
+++ emacs/main.el
@@ -1,3 +1,9 @@
-(load "~/Code/RaySystem/emacs/ray-info.el")
+(let* ((current-file (or load-file-name buffer-file-name))
+       (current-dir (file-name-directory current-file))
+       (el-files (directory-files current-dir t "\\.el$")))
+  (dolist (file el-files)
+    (unless (equal file current-file)
+      (load file))))
 
+;; Still explicitly require any specific features needed
 (require 'ray-info)
