223bc7b7350a59772b107fa631263e7600985130
Maxiee <maxieewong@gmail.com>
2024-12-22 21:42:54 +0800

    Add Channel model and establish relationships with Site and Info models

--- raysystem/module/info/model.py
+++ raysystem/module/info/model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
+from sqlalchemy import table
 from sqlmodel import Field, SQLModel, Relationship
 
 
@@ -13,6 +14,18 @@ class Site(SQLModel, table=True):
     rss: Optional[str] = None
 
     infos: list["Info"] = Relationship(back_populates="site")
+    channels: list["Channel"] = Relationship(back_populates="site")
+
+class Channel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    url: Optional[str] = None
+    site_id: Optional[int] = Field(default=None, foreign_key="site.id")
+    rss: Optional[str] = None
+    
+    site: Optional["Site"] = Relationship(back_populates="channels")
+    
+    infos: list["Info"] = Relationship(back_populates="channel")
 
 
 class Info(SQLModel, table=True):
@@ -24,9 +37,11 @@ class Info(SQLModel, table=True):
     description: Optional[str] = None
     image: Optional[bytes] = None
     site_id: Optional[int] = Field(default=None, foreign_key="site.id")
+    channel_id: Optional[int] = Field(default=None, foreign_key="channel.id")
     is_new: bool = Field(default=True)
     is_mark: bool = Field(default=False)
 
     site: Optional["Site"] = Relationship(back_populates="infos")
+    channel: Optional["Channel"] = Relationship(back_populates="infos")
 
 
