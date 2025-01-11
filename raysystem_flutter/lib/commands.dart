5cbedc04b3e3f4c363a10f27e10cfdf521e95e9e
Maxiee <maxieewong@gmail.com>
2025-01-11 16:43:36 +0800

    Add command structure for note and todo applications with dynamic callbacks

---
+++ raysystem_flutter/lib/commands.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+Map<String, dynamic> commands = {
+  'commands': [
+    {
+      'command': 'note-app',
+      'title': '笔记应用',
+      'icon': Icons.note,
+      'commands': [
+        {
+          'command': 'note-add',
+          'title': '添加笔记',
+          'icon': Icons.add,
+          'callback': () {
+            print('添加笔记');
+          }
+        },
+        {
+          'command': 'note-list',
+          'title': '查看笔记',
+          'icon': Icons.list,
+          'callback': () {
+            print('查看笔记');
+          }
+        },
+        {
+          'command': 'note-delete',
+          'title': '删除笔记',
+          'icon': Icons.delete,
+          'callback': () {
+            print('删除笔记');
+          }
+        }
+      ]
+    },
+    {
+      'command': 'todo-app',
+      'title': '待办事项应用',
+      'icon': Icons.check,
+      'commands': [
+        {
+          'command': 'todo-add',
+          'title': '添加待办事项',
+          'icon': Icons.add,
+          'callback': () {
+            print('添加待办事项');
+          }
+        },
+        {
+          'command': 'todo-list',
+          'title': '查看待办事项',
+          'icon': Icons.list,
+          'callback': () {
+            print('查看待办事项');
+          }
+        },
+        {
+          'command': 'todo-delete',
+          'title': '删除待办事项',
+          'icon': Icons.delete,
+          'callback': () {
+            print('删除待办事项');
+          }
+        },
+        {
+          'command': 'todo-more',
+          'title': '更多操作',
+          'icon': Icons.more_horiz,
+          'commands': [
+            {
+              'command': 'todo-more-1',
+              'title': '更多操作1',
+              'icon': Icons.more_horiz,
+              'callback': () {
+                print('更多操作1');
+              }
+            },
+            {
+              'command': 'todo-more-2',
+              'title': '更多操作2',
+              'icon': Icons.more_horiz,
+              'callback': () {
+                print('更多操作2');
+              }
+            }
+          ]
+        }
+      ]
+    },
+  ]
+};
--- raysystem_flutter/lib/main.dart
+++ raysystem_flutter/lib/main.dart
@@ -2,101 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:raysystem_flutter/card/card_list_view.dart';
 import 'package:raysystem_flutter/card/card_manager.dart';
+import 'package:raysystem_flutter/commands.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
       create: (_) => CardManager(maxCards: 20), child: const MyApp()));
 }
-
-Map<String, dynamic> commands = {
-  'commands': [
-    {
-      'command': 'note-app',
-      'title': '笔记应用',
-      'icon': Icons.note,
-      'commands': [
-        {
-          'command': 'note-add',
-          'title': '添加笔记',
-          'icon': Icons.add,
-          'callback': () {
-            print('添加笔记');
-          }
-        },
-        {
-          'command': 'note-list',
-          'title': '查看笔记',
-          'icon': Icons.list,
-          'callback': () {
-            print('查看笔记');
-          }
-        },
-        {
-          'command': 'note-delete',
-          'title': '删除笔记',
-          'icon': Icons.delete,
-          'callback': () {
-            print('删除笔记');
-          }
-        }
-      ]
-    },
-    {
-      'command': 'todo-app',
-      'title': '待办事项应用',
-      'icon': Icons.check,
-      'commands': [
-        {
-          'command': 'todo-add',
-          'title': '添加待办事项',
-          'icon': Icons.add,
-          'callback': () {
-            print('添加待办事项');
-          }
-        },
-        {
-          'command': 'todo-list',
-          'title': '查看待办事项',
-          'icon': Icons.list,
-          'callback': () {
-            print('查看待办事项');
-          }
-        },
-        {
-          'command': 'todo-delete',
-          'title': '删除待办事项',
-          'icon': Icons.delete,
-          'callback': () {
-            print('删除待办事项');
-          }
-        },
-        {
-          'command': 'todo-more',
-          'title': '更多操作',
-          'icon': Icons.more_horiz,
-          'commands': [
-            {
-              'command': 'todo-more-1',
-              'title': '更多操作1',
-              'icon': Icons.more_horiz,
-              'callback': () {
-                print('更多操作1');
-              }
-            },
-            {
-              'command': 'todo-more-2',
-              'title': '更多操作2',
-              'icon': Icons.more_horiz,
-              'callback': () {
-                print('更多操作2');
-              }
-            }
-          ]
-        }
-      ]
-    },
-  ]
-};
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -145,7 +56,7 @@ class _MyAppState extends State<MyApp> {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         ...currentCommands.map((cmd) {
-          return ElevatedButton(
+          return ElevatedButton.icon(
             onPressed: () {
               if (cmd.containsKey('commands')) {
                 final List<dynamic>? subCmds = cmd['commands'];
@@ -158,7 +69,8 @@ class _MyAppState extends State<MyApp> {
                 cmd['callback']();
               }
             },
-            child: Text(cmd['title']?.toString() ?? '无标题'),
+            icon: Icon(cmd['icon'] as IconData?),
+            label: Text(cmd['title']?.toString() ?? '无标题'),
           );
         }),
         if (_commandStack.length > 1)
