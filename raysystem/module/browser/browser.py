e220ef267ecbaca8aedae7cec11b88a9d6e1d7ef
Maxiee <maxieewong@gmail.com>
2025-01-12 14:31:12 +0800

    Refactor browser opening logic to use async context manager and improve page load handling

--- raysystem/module/browser/browser.py
+++ raysystem/module/browser/browser.py
@@ -1,10 +1,15 @@
 from playwright.async_api import async_playwright
 
 async def open_browser():
-    playwright = await async_playwright().start()  # 手动启动 Playwright
-    browser = await playwright.chromium.launch(
-        channel="msedge", 
-        headless=False,
+    async with async_playwright() as p:
+        browser = await p.chromium.connect_over_cdp(
+            "http://localhost:9222",
         )
-    page = await browser.new_page()
-    await page.goto("https://maxieewong.com/")
+        page = await browser.new_page()
+        print('ready to open page')
+        await page.goto("https://maxieewong.com/")
+        print('page opened')
+        await page.wait_for_load_state("domcontentloaded")
+        print(await page.title())
+        # sleep 10 seconds, then close the browser
+        await page.wait_for_timeout(10000)
