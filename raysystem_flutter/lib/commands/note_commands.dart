0a4794f4bec44d492d380de6c8ffdaece7ed7440
Maxiee <maxieewong@gmail.com>
2025-04-13 14:56:05 +0800

    feat: Adjust note tree card height dynamically based on screen size

--- raysystem_flutter/lib/commands/note_commands.dart
+++ raysystem_flutter/lib/commands/note_commands.dart
@@ -21,7 +21,7 @@ final noteCommands = Command(
       callback: (context, cardManager) async {
         cardManager.addCard(
             SizedBox(
-              height: 400,
+              height: MediaQuery.of(context).size.height - 200,
               child: NoteTreeCard(
                 treeService: ApiNoteTreeService(notesApi: notesApi),
               ),
