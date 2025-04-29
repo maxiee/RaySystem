90ec33fcbc0545c9fa0a0a1ecd9e724af00cbb76
Maxiee <maxieewong@gmail.com>
2025-04-30 00:18:27 +0800

    refactor(note_tree): 移除冗余的辅助方法，简化 NoteTree 类结构

--- raysystem_flutter/lib/module/note/components/note_tree/notifier/note_tree.dart
+++ raysystem_flutter/lib/module/note/components/note_tree/notifier/note_tree.dart
@@ -1,5 +1,4 @@
 // filepath: /Volumes/ssd/Code/RaySystem/raysystem_flutter/lib/module/note/components/note_tree/notifier/note_tree.dart
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:raysystem_flutter/module/note/components/note_tree/notifier/tree_facade.dart';
 import 'package:raysystem_flutter/module/note/components/note_tree/notifier/tree_state.dart';
@@ -49,49 +48,4 @@ class NoteTree extends AsyncNotifier<NoteTreeState> {
           {bool preserveExpansion = false, Set<int>? ensureExpanded}) =>
       ref.read(noteTreeFacadeProvider.notifier).refreshTree(
           preserveExpansion: preserveExpansion, ensureExpanded: ensureExpanded);
-
-  // --- Helper methods ---
-  NoteTreeItem? _findItemById(List<NoteTreeItem> items, int id) =>
-      ref.read(noteTreeFacadeProvider.notifier).findItemById(id);
-
-  // 下面的方法保持原有的签名，但委托给新架构中的对应方法
-  Future<void> _loadChildrenAndUpdateState(int parentId) async {
-    // 这个方法在新架构中由 toggleExpand 内部处理
-    await toggleExpand(
-        NoteTreeItem(id: parentId, name: "临时名称", isFolder: true));
-  }
-
-  List<NoteTreeItem>? _updateNodeChildren(List<NoteTreeItem> currentItems,
-      int parentId, List<NoteTreeItem> newChildren) {
-    // 这个方法在新架构中由 TreeBaseNotifier 处理
-    // 我们在这里提供一个实现，以保持API兼容性，但它实际上不会被调用
-    return null;
-  }
-
-  Future<bool> _isDescendantOf(int itemId, int potentialAncestorId) async {
-    // 这个方法在新架构中由 TreeDragDropNotifier 处理
-    // 提供一个实现以保持API兼容性
-    return false;
-  }
-
-  Future<NoteTreeState> _recursivelyLoadChildrenForState(
-      NoteTreeState currentState, Set<int> expandedIdsToLoad) async {
-    // 这个方法在新架构中由 TreeFacade 和 TreeExpansionNotifier 处理
-    // 提供一个实现以保持API兼容性
-    return currentState;
-  }
-
-  void _findFoldersToLoadRecursive(
-      List<NoteTreeItem> items, Set<int> expandedIds, List<int> foldersToLoad) {
-    // 这个方法在新架构中由 TreeExpansionNotifier 处理
-    // 提供一个实现以保持API兼容性
-  }
-
-  List<NoteTreeItem> _updateMultipleNodesChildren(
-      List<NoteTreeItem> currentItems,
-      Map<int, List<NoteTreeItem>> childrenMap) {
-    // 这个方法在新架构中由 TreeBaseNotifier 处理
-    // 提供一个实现以保持API兼容性
-    return currentItems;
-  }
 }
