fd7942fbc73f46fd728803d7275768871c32d0de
Maxiee <maxieewong@gmail.com>
2025-05-10 14:53:30 +0800

    feat(people_model): 添加人物名称表及其与人物的关系

--- raysystem/module/people/model.py
+++ raysystem/module/people/model.py
@@ -1,29 +1,37 @@
 from typing import Optional
-from sqlalchemy import Integer, String, Date
+from sqlalchemy import Integer, String, Date, ForeignKey
 from module.db.base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class PeopleName(Base):
+    """
+    人物名称表
+    """
+
+    __tablename__ = "people_name"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=False)
+    name: Mapped[str] = mapped_column(String, index=True, nullable=False)
+
+    # Relationship with People
+    people = relationship("People", back_populates="names")
 
 
 class People(Base):
     """
     人物
     """
+
     __tablename__ = "people"
-    # 人物 id
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    # 人物名称
-    name: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=True)
-    # 人物名称2
-    name2: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=True)
-    # 人物名称3
-    name3: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=True)
-    # 人物名称4
-    name4: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=True)
-    # 人物名称5
-    name5: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=True)
-    # 人物描述
     description: Mapped[Optional[str]] = mapped_column(String, nullable=True)
-    # 人物头像
     avatar: Mapped[Optional[str]] = mapped_column(String, nullable=True)
-    # 人物出生日期
     birth_date: Mapped[Optional[Date]] = mapped_column(Date, nullable=True)
+
+    # Relationship with PeopleName
+    names = relationship(
+        "PeopleName", back_populates="people", cascade="all, delete-orphan"
+    )
